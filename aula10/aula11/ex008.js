e68b3acfce3e8aed0a650fea77a76e80f164ba15
unknown <manuogg90@gmail.com>
2021-06-25 10:47:53 -0300

    feat: condições simples

---
+++ aula10/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro foi de ${vel} km por hora`)
+if (vel > 60) { //condição simples
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('dirija sempre usando cinto de segurança')
