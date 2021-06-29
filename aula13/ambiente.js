45c9cbca26471324559b51382ac881f7543b828c
unknown <manuogg90@gmail.com>
2021-06-29 14:14:22 -0300

    feat: repetição com while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+// PRIMEIRO ESTRUTURA DE REPETIÇÃO - BÁSICA:
+
+// var contador = 1;
+
+// while (contador <= 6) {
+//   console.log(`Passo ${contador}`);
+//   contador++
+// }
+
+// SEGUNDA ESTRUTURA DE REPETIÇÃO - DO:
+
+var contador = 1;
+
+do {
+  console.log(`Passo${contador}`);
+  contador++
+} while (contador <= 6);
