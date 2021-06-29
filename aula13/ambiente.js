040fe5c1506184614ea21193eeeec5e6b7974309
unknown <manuogg90@gmail.com>
2021-06-29 14:39:48 -0300

    feat: repetição com for

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,4 +1,4 @@
-// PRIMEIRO ESTRUTURA DE REPETIÇÃO - BÁSICA:
+// PRIMEIRO ESTRUTURA DE REPETIÇÃO - WHILE:
 
 // var contador = 1;
 
@@ -9,9 +9,15 @@
 
 // SEGUNDA ESTRUTURA DE REPETIÇÃO - DO:
 
-var contador = 1;
+// var contador = 1;
 
-do {
-  console.log(`Passo${contador}`);
-  contador++
-} while (contador <= 6);
+// do {
+//   console.log(`Passo${contador}`);
+//   contador++
+// } while (contador <= 6);
+
+// TERCEIRA ESTRUTURA DE REPETIÇÃO - FOR:
+
+for (var contador = 1; contador <= 10; contador++) {
+  console.log(contador)
+}
